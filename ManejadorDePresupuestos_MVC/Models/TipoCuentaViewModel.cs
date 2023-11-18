38515a48c7a02708af80e20608bc6640f1f8eda8
CarlosCode502 <carloshl.code21@gmail.com>
2023-11-17 22:58:37 -0600

    Validaciones por defecto.

--- ManejadorDePresupuestos_MVC/Models/TipoCuentaViewModel.cs
+++ ManejadorDePresupuestos_MVC/Models/TipoCuentaViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 using System.Timers;
 
 namespace ManejadorDePresupuestos_MVC.Models
@@ -20,6 +22,22 @@ namespace ManejadorDePresupuestos_MVC.Models
         [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe ser mayor a {2} y menor a {1}")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
-        public int Orden { get; set; }       
+        public int Orden { get; set; }
+
+        /*PRUEBAS DE VALIDACIONES POR DEGECTO*/
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [EmailAddress(ErrorMessage = "El campo debe ser un {0} válido")]
+        [DisplayName("Correo electrónico")]
+        public string Email { get; set; }
+
+        [Range(minimum: 18, maximum: 100, ErrorMessage = "El valor debe estar entre {1} y {2}")]
+        public int Edad { get; set; }
+
+        [Url(ErrorMessage = "Debe ser una {0} válida")]
+        public string URL { get; set; }
+
+        [CreditCard(ErrorMessage = "La Tarjeta de crédito no es válida")]
+        [DisplayName("Tarjeta de Crédito")]
+        public string TarjetaDeCredito { get; set; }
     }
 }
