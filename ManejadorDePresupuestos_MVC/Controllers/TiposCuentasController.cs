fde2fc2091a09588ffdccd66bd15152ddd075cff
CarlosCode502 <carloshl.code21@gmail.com>
2023-11-17 20:24:51 -0600

    Se creó la BD en SQL Server.
    Agregando a Tipos de Cuentas:
    1. Controlador.
    2. Modelo.
    3. Vista.
    
    V#100 Formulario

---
+++ ManejadorDePresupuestos_MVC/Controllers/TiposCuentasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManejadorDePresupuestos_MVC.Controllers
+{
+    //V#100 FORMULARIO TIPO DE CUENTAS 
+    //Controlador que va a servir para manejar las multiples cuentas que puede tener
+    //un usuario
+    public class TiposCuentasController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
---
+++ ManejadorDePresupuestos_MVC/Models/TipoCuentaViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ManejadorDePresupuestos_MVC.Models
+{
+    //V# 100 FORMULARIO TIPOS CUENTAS
+    //Creando el modelo de Tipos de cuentas
+
+    /// <summary>
+    /// Contiene los campos de la tabla Tbl_TiposCuentas_Sys.
+    /// </summary>
+    public class TipoCuentaViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }       
+    }
+}
