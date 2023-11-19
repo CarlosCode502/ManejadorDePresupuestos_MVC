3d0e36c705f11f48ecf85361eef95b402c647895
CarlosCode502 <carloshl.code21@gmail.com>
2023-11-18 19:31:06 -0600

    Validaciones por defecto.

--- ManejadorDePresupuestos_MVC/Models/TipoCuentaViewModel.cs
+++ ManejadorDePresupuestos_MVC/Models/TipoCuentaViewModel.cs
@@ -15,31 +15,35 @@ namespace ManejadorDePresupuestos_MVC.Models
     {
         //V#101 VALIDANDO EL FORMULARIO( Nunca confiar en la data que envía el usuario)
 
+        //Id ya que sera necesario interactuar con la bd
         public int Id { get; set; }
 
         //V#102 Visualizando los Errores de validación
-        [Required(ErrorMessage = "* El campo {0} es obligatorio.")]
+        [Required(ErrorMessage = "* El campo {0} es requerido.")]
         [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe ser mayor a {2} y menor a {1}")]
         public string Nombre { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "* El campo {0} es requerido.")]
         public int UsuarioId { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "* El campo {0} es requerido.")]
         public int Orden { get; set; }
 
-        /*PRUEBAS DE VALIDACIONES POR DEGECTO*/
-        [Required(ErrorMessage = "El campo {0} es requerido.")]
-        [EmailAddress(ErrorMessage = "El campo debe ser un {0} válido")]
-        [DisplayName("Correo electrónico")]
-        public string Email { get; set; }
+        ////V#104 OTRAS VALIDACIONES POR DEFECTO
+        ///*Pruebas de validaciones por degecto*/
+        //[Required(ErrorMessage = "* El campo {0} es requerido.")]
+        //[EmailAddress(ErrorMessage = "El campo debe ser un {0} válido")]
+        //[DisplayName("Correo electrónico")]
+        //public string Email { get; set; }
 
-        [Range(minimum: 18, maximum: 100, ErrorMessage = "El valor debe estar entre {1} y {2}")]
-        public int Edad { get; set; }
+        //[Range(minimum: 18, maximum: 100, ErrorMessage = " El valor debe estar entre {1} y {2}")]
+        //public int Edad { get; set; }
 
-        [Url(ErrorMessage = "Debe ser una {0} válida")]
-        public string URL { get; set; }
+        //[Url(ErrorMessage = "Debe ser una {0} válida")]
+        //public string URL { get; set; }
 
-        [CreditCard(ErrorMessage = "La Tarjeta de crédito no es válida")]
-        [DisplayName("Tarjeta de Crédito")]
-        public string TarjetaDeCredito { get; set; }
+        //[CreditCard(ErrorMessage = "La Tarjeta de crédito no es válida")]
+        //[DisplayName("Tarjeta de Crédito")]
+        //public string TarjetaDeCredito { get; set; }
     }
 }
