7a594a3112136c5e5d9b864ff405c10a492c44f4
CarlosCode502 <carloshl.code21@gmail.com>
2023-11-19 15:46:57 -0600

    Instalando Dapper y MSqlClient (Error de certificado Sql).
    Solución temporal agregar a la cadena de conexion esto:
    TrustServerCertificate=True

--- ManejadorDePresupuestos_MVC/Controllers/TiposCuentasController.cs
+++ ManejadorDePresupuestos_MVC/Controllers/TiposCuentasController.cs
@@ -1,5 +1,7 @@
-﻿using ManejadorDePresupuestos_MVC.Models;
+﻿using Dapper;
+using ManejadorDePresupuestos_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ManejadorDePresupuestos_MVC.Controllers
 {
@@ -8,6 +10,23 @@ namespace ManejadorDePresupuestos_MVC.Controllers
     //un usuario
     public class TiposCuentasController : Controller
     {
+
+        #region //V#109 Comunicandonos con la Base de datos - Connection Strings (Realizando un prueba de conexion)
+        /// <summary>
+        /// Campo para asignar la cadena de conexion
+        /// </summary>
+        private readonly string connectionString;
+
+        /// <summary>
+        /// Constructor que contiene la Interfaz IConfiguration.
+        /// </summary>
+        /// <param name="configuration">Recibe o obtiene la Cadena de conexión de los perfiles de configuración</param>
+        public TiposCuentasController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection"); //Igual que en proveedores de configuración
+        }
+        #endregion
+
         /// <summary>
         /// Todas las peticiones Get(Muestra la vista)
         /// </summary>
@@ -15,9 +34,15 @@ namespace ManejadorDePresupuestos_MVC.Controllers
         [HttpGet]
         public IActionResult Crear()
         {
-            return View();
-        }
+            //Abrimos la conexión (pasamos la cadena de conexion obtenuda en el constructor)
+            using (var connection = new SqlConnection(connectionString))
+            {
+                //Ejecutamos una consulta a la bd con la palabra Query(Importa y utiliza Dapper)
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+            }
 
+                return View();
+        }
         
         [HttpPost]
         public IActionResult Crear(TipoCuentaViewModel tipoCuentaViewModel)
