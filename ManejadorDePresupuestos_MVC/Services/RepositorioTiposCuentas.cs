610b5234636dd19c3e0cb038cbf3442a1da95b59
CarlosCode502 <carloshl.code21@gmail.com>
2023-11-19 23:00:33 -0600

    Validando que el Usuario no pueda registrar el mismo tipocuenta.

--- ManejadorDePresupuestos_MVC/Services/RepositorioTiposCuentas.cs
+++ ManejadorDePresupuestos_MVC/Services/RepositorioTiposCuentas.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using ManejadorDePresupuestos_MVC.Models;
 using Microsoft.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ManejadorDePresupuestos_MVC.Services
 {
@@ -61,12 +62,26 @@ namespace ManejadorDePresupuestos_MVC.Services
             //Consulta que obtiene el id(int) del registro
             //Dapper -> QuerySingle (Realiza un query que va a obtener un solo resultado). 
             var id = await connection.QuerySingleAsync<int>
-                ($@"INSERT INTO Tbl_TiposCuentas_Sys (Nombre, UsuarioId, Orden)
+                (@"INSERT INTO Tbl_TiposCuentas_Sys (Nombre, UsuarioId, Orden)
                     Values (@Nombre, @UsuarioId, 0);
                     SELECT SCOPE_IDENTITY();", tipoCuentaViewModel); //SCOPE_IDENTITY() -> Trae el id del registro creado
 
             //Asigna el id obtenido al campo id del modelo tipo cuenta 
             tipoCuentaViewModel.Id = id;
+        }
+
+        //V# 113 Validaciones personalizadas a Nivel del Controlador 
+        //Creando un método que valide si existe un TipoCuenta para determinado usuario
+        public async Task<bool> Existe(string nombre, int usuarioId)
+        {
+            using var connection2 = new SqlConnection(connectionString);
+
+            var existe = await connection2.QueryFirstOrDefaultAsync<int>
+                (@"SELECT 1 
+                FROM Tbl_TiposCuentas_Sys
+                WHERE Nombre = @Nombre AND UsuarioId = @UsuarioId;", new {nombre, usuarioId});
+
+            return existe == 1;
         }
     }
 }
